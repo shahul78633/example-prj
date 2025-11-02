79264e04d7c55561e67f68ccb65192399c9f491d
SHAHUL HAMEED KHAN.M <shahulhamedkhanm@gmail.com>
2025-11-02 14:27:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
